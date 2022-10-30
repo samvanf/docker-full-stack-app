afb07278d30e20687462b2449de9ec3d5b498dec
Sam VanFossen <samvanf@gmail.com>
2022-10-30 19:32:38 -0400

    Fixing compile error

---
+++ packages/react-app/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Example from "./Example";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Example />
+        </QueryClientProvider>
+    )
+}
